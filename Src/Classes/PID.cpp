31b7b1e05f0a367a5f80ff35fbcd0ed43641cc7b
Martiszka <Martiszkaaa@gmail.com>
2020-03-05 19:27:57 +0100

    Dodnie klasy pid

---
+++ Src/Classes/PID.cpp
@@ -0,0 +1,18 @@
+/*
+ * PID.cpp
+ *
+ *  Created on: 05.03.2020
+ *      Author: Marta
+ */
+
+#include <PID.h>
+
+PID::PID() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PID::~PID() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Src/Classes/PID.h
@@ -0,0 +1,17 @@
+/*
+ * PID.h
+ *
+ *  Created on: 05.03.2020
+ *      Author: Marta
+ */
+
+#ifndef CLASSES_PID_H_
+#define CLASSES_PID_H_
+
+class PID {
+public:
+	PID();
+	virtual ~PID();
+};
+
+#endif /* CLASSES_PID_H_ */
